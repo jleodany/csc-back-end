65faeee6e3aec78e15ac582a92b639ee091d5a41
jleodany <joseleodany@gmail.com>
2018-09-16 18:05:43 -0400

    First commit, just getting started

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var app = express();
+var mysql = require("mysql")
+// var connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'node_mysql',
+//   port: 3306
+// });
+// connection.connect(function (error) {
+//   if (error) {
+//     throw error;
+//   } else {
+//     console.log('Conexion correcta.');
+//   }
+// });
+
+app.get("/test", function (req, res) {
+  console.log("Petición Recibida");
+  res.send("HELLOW FRONT");
+})
+
+app.listen(3080, () => {
+  console.log("Server running on port 3080");
+});

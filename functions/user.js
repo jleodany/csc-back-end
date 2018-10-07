5196d058619640d236faed0462a0ced52c3f06c2
jleodany <joseleodanygmail.com>
2018-10-07 19:59:19 -0400

    Validacion para que no permita usuarios repetidos.

--- functions/user.js
+++ functions/user.js
@@ -1,45 +1,50 @@
-const {getUserByUserName} = require('./globals/common')
+const { getUserByUserName } = require('./globals/common')
 
 const mysql = require("mysql");
 
-exports.registerUser = (req, res) => {
+exports.registerUser = async/*<--- importante para usar 'await'*/ (req, res) => {
   const userName = req.body.userName;
   const pass = req.body.pass;
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
   const email = req.body.email;
   const type = req.body.type;
+  const dbUser = await getUserByUserName(userName)
   if (!userName || !pass || !firstName || !lastName || !email || !type) {
     return res.json({ status: 400, message: "Faltan Datos Obligatorios", succes: false });
   }
-  const values = [[null, userName, pass, firstName, lastName, email, type]]
-  const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'csc'
-  });
-  connection.connect(function (error) {
-    if (error) {
-      console.log("ERROR>", error);
-    } else {
-      console.log('Conexion correcta.');
-    }
-  });
-  connection.query('INSERT INTO users (id, userName, pass, firstName, lastName, email, type) VALUES?',
-    [values], function (error, result) {
-      console.log("INSIDE INSERT FUNCTION");
+  if (dbUser.length > 0) {
+    return res.json({ status: 400, message: "Usuario Ya Registrado", succes: false })
+  } else {
+    const values = [[null, userName, pass, firstName, lastName, email, type]]
+    const connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'csc'
+    });
+    connection.connect(function (error) {
       if (error) {
-        console.log("ERROR", error);
-        return res.json({ status: 400, message: "Error en Inserción de Datos", succes: false })
+        console.log("ERROR>", error);
       } else {
-        console.log("SUCCEED");
-        return res.json({ status: 200, message: "Usuario Registrado Exitosamente", succes: true });
+        console.log('Conexion correcta.');
       }
     });
-  connection.end()
-  console.log("registerUser", req.body);
-  // res.send("Ready")
+    connection.query('INSERT INTO users (id, userName, pass, firstName, lastName, email, type) VALUES?',
+      [values], function (error, result) {
+        console.log("INSIDE INSERT FUNCTION");
+        if (error) {
+          console.log("ERROR", error);
+          connection.end()
+          return res.json({ status: 400, message: "Error en Inserción de Datos", succes: false })
+        } else {
+          console.log("SUCCEED");
+          connection.end()
+          return res.json({ status: 200, message: "Usuario Registrado Exitosamente", succes: true });
+        }
+      }
+    );
+  }
 }
 
 exports.getUsers = (req, res) => {
@@ -65,9 +70,9 @@ exports.login = async (req, res) => {
   const userName = req.body.userName;
   const pass = req.body.pass;
   const dbUser = await getUserByUserName(userName)
-  if(dbUser.length == 0){
+  if (dbUser.length == 0) {
     return res.json({ status: 400, message: "El Usuario Ingresado no Existe", succes: false })
-  } else if(dbUser[0].pass != pass){
+  } else if (dbUser[0].pass != pass) {
     return res.json({ status: 400, message: "Contraseña Inválida", succes: false })
   } else {
     return res.json({ status: 200, message: "Usuario Logueado Correctamente", succes: true })

bd11e8bc2380d15edd3bf11d6ae872adb184209d
jleodany <jleodany@gmail.com>
2018-11-04 22:29:26 -0400

    BBDD

--- DB/csc.sql
+++ DB/csc.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.2
--- http://www.phpmyadmin.net
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
 --
--- Servidor: localhost
--- Tiempo de generación: 04-11-2018 a las 16:18:25
--- Versión del servidor: 10.1.13-MariaDB
--- Versión de PHP: 5.6.20
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 05-11-2018 a las 03:28:44
+-- Versión del servidor: 10.1.36-MariaDB
+-- Versión de PHP: 7.2.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -58,8 +60,8 @@ CREATE TABLE `casos` (
 --
 
 INSERT INTO `casos` (`idCaso`, `asunto`, `descripcion`, `f_apertura`, `user`, `userName`, `operador`, `operadorName`, `status`, `type`) VALUES
-(2, 'Prueba ', 'Primera prueba de crear caso', 1540958400000, 4, 'jleodany', NULL, '', '0', 'Incidente'),
-(3, 'Prueba campos nuevos', 'Esta es una prueba para los nuevos campos agregados a la tabla', 1541304000000, 4, 'jleodany', NULL, NULL, '0', 'Incidente');
+(2, 'Prueba ', 'Primera prueba de crear caso', 1540958400000, 4, 'jleodany', 5, 'itsyasus', '0', 'Incidente'),
+(3, 'Prueba campos nuevos', 'Esta es una prueba para los nuevos campos agregados a la tabla', 1541304000000, 4, 'jleodany', 5, 'itsyasus', '0', 'Incidente');
 
 -- --------------------------------------------------------
 
@@ -81,7 +83,9 @@ INSERT INTO `session` (`id`, `token`, `user`) VALUES
 (5, '1540956885496', 4),
 (6, '1540980056119', 4),
 (9, '1540981248238', 4),
-(10, '1541339416184', 4);
+(10, '1541339416184', 4),
+(12, '1541379512503', 5),
+(14, '1541384465406', 4);
 
 -- --------------------------------------------------------
 
@@ -106,7 +110,9 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `userName`, `pass`, `firstName`, `lastName`, `email`, `type`) VALUES
 (4, 'jleodany', 'eyJ3b3JkIjoiJDJiJDA0JGszQzBiOVVXdThpMEYvU3VULzQ2WHVJY0xpS05uSUtlWWVMMlloaTlOVzVINGs1U25aLy5pIiwic2VjcmV0V29yZCI6IjEyMC4xMTguOTkuMTIxLjk5LjEwOS40OC4xMDYuMTEwLjEyMSJ9', 'Jose', 'Ramirez', 'joseleodany@gmail.com', 1),
 (5, 'itsyasus', 'eyJ3b3JkIjoiJDJiJDA0JE5UNlpHa1V0MkdkemNDYXVyaVlzUHVUMG5FcktHL2d2elRLSVJZLnNIakouMS9JdFpvN2RHIiwic2VjcmV0V29yZCI6IjUxLjQ5LjQ4LjExNi4xMTEuMTE0LjQ5Ljk5LjUzLjEwOCJ9', 'Jesus', 'Soto', 'jesusenriquesotoa@gmail.com', 2),
-(6, 'kinanActivo', 'eyJ3b3JkIjoiJDJiJDA5JEJwcTEzNTNhYnRjN0ZzUlg1bEJ3UC52Z2RJM0hRWlU4TmVxU09MOU5tSXJhYnZFWEh0cnVXIiwic2VjcmV0V29yZCI6IjExOS4xMTUuNTcuNTQuMTExLjExMy4xMTQuMTE2LjU0LjExNyJ9', 'Kinan', 'El Dhibal', 'kinan05@gmail.com', 3);
+(6, 'kinanActivo', 'eyJ3b3JkIjoiJDJiJDA5JEJwcTEzNTNhYnRjN0ZzUlg1bEJ3UC52Z2RJM0hRWlU4TmVxU09MOU5tSXJhYnZFWEh0cnVXIiwic2VjcmV0V29yZCI6IjExOS4xMTUuNTcuNTQuMTExLjExMy4xMTQuMTE2LjU0LjExNyJ9', 'Kinan', 'El Dhibal', 'kinan05@gmail.com', 3),
+(7, 'levij', 'eyJ3b3JkIjoiJDJiJDA3JE92N2NIbTNiRDh2YlpTY1VCVVhXWmV5TTV2WmlldS9NMHJ4a3FDbkxvV2tSVG9QZ2ovazBtIiwic2VjcmV0V29yZCI6IjExNy41My4xMDEuMTA2LjU1LjU3LjQ4LjQ4LjExMy4xMTcifQ==', 'Levi', 'Ramirez', 'leviramirez@gmail.com', 2),
+(8, 'leurisj', 'eyJ3b3JkIjoiJDJiJDA5JG9vb0d0YjdpMFIya1h4Yklab1hFb080R2RTeHYxakpZSnI0ODBGWW1JT2RsRVZBWFlUQml5Iiwic2VjcmV0V29yZCI6IjEwNy4xMTQuNTQuNDkuNTcuNTYuNTEuNTEuMTIwLjEyMCJ9', 'Leuris', 'Ramirez', 'leurisramirez@gmail.com', 2);
 
 --
 -- Índices para tablas volcadas
@@ -146,21 +152,26 @@ ALTER TABLE `users`
 --
 ALTER TABLE `adjuntos`
   MODIFY `idAdjunto` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT de la tabla `casos`
 --
 ALTER TABLE `casos`
   MODIFY `idCaso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=349;
+
 --
 -- AUTO_INCREMENT de la tabla `session`
 --
 ALTER TABLE `session`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
